93a215b610e1e24b741b684bfd2c4ee7652a7090
Kenta Funada <kenta.funada2@gmail.com>
2024-03-18 17:37:58 -0400

    simplified

--- test_sql.py
+++ test_sql.py
@@ -1,26 +1,20 @@
 import mysql.connector
 
+# Connect to MySQL
 db = mysql.connector.connect(
     host="localhost",
     user="kfunada",
     passwd="Kf100219980929!",
     database="testdatabase",
 )
+
+# Create a cursor object
 mycursor = db.cursor()
 
-
-# Check if the table exists
-mycursor.execute("SHOW TABLES LIKE 'Person'")
-table_exists = mycursor.fetchone()
-
-# If the table doesn't exist, create it
-if not table_exists:
-    mycursor.execute(
-        "CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID INT PRIMARY KEY AUTO_INCREMENT)"
-    )
-    print("Table 'Person' created successfully.")
-else:
-    print("Table 'Person' already exists.")
+# Create the 'Person' table
+mycursor.execute(
+    "CREATE TABLE IF NOT EXISTS Person (name VARCHAR(50), age smallint UNSIGNED, personID INT PRIMARY KEY AUTO_INCREMENT)"
+)
 
 # Describe the table
 mycursor.execute("DESCRIBE Person")
